0f8276ce98103c1aa4b9fc670cc7152c95453ee6
TikrA124 <122836592+TikrA124@users.noreply.github.com>
2023-01-16 17:14:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,239 @@
+// var firstname= "Tikristi" 
+// var lastname= "Aderinwale"
+// console.log(firstname)
+// console.log(lastname)
+// var phonenumber= "123-456789"
+// var firstnumber= 1
+// var secondnumber= 2
+// console.log(firstnumber+secondnumber)
+// var thirdnumber;
+// console.log(thirdnumber)
+// var firstnumber= 9
+// // firstnumber+=10
+// console.log(firstnumber)
+// console.log(typeof firstnumber)
+// var firstname= "Tikristi"
+// console.log(typeof firstname)
+// var secondnumber= 2
+// console.log(firstnumber%secondnumber)
+// var hours= 1000
+// var days= 1000/24
+// days= Math.floor(days)
+// console.log(days)
+// var hoursleft= 1000%24
+// console.log(hoursleft)
+// var days= 450
+// var weeks=450/
+// weeks= Math.floor(weeks)
+// console.log(weeks)
+// var daysleft=450%7
+// console.log(daysleft)
+// var firstnumber= 2
+// var secondnumber= "2"
+// console.log(firstnumber===secondnumber)
+// var firstnumber=9
+// var secondnumber=12
+// console.log(firstnumber>10 && secondnumber<100)
+// var fullname= prompt("What is your full name?")
+// var password= prompt("What is your password?")
+// if (password== "12345") {
+//     console.log("Access Granted!")
+// } else {
+//     console.log("Access Denied!")
+// }
+// var age= prompt("How old are you?")
+// if (age<20) {
+//     console.log("Access Granted!")
+// } else {
+//     console.log("Access Denied!")
+// }
+// var fullname= prompt("What is your full name?")
+// var bestfood= prompt("What is your best food?")
+// switch(bestfood){
+//     case "rice":
+//         console.log(fullname+" your best food is "+bestfood+" this is a carbohydrate food. ")
+//         break;
+//     case "beans":
+//         console.log(fullname+" your best food is "+ bestfood+" this is a proteinous food.")
+//         break;
+//     case "apple":
+//         console.log(fullname+" your best food is "+ bestfood+" this is a fruit.")
+//         break;
+//     case "fish":
+//         console.log(fullname+" your best food is "+ bestfood+" this is a type of fat.")
+//         break;
+//     default:
+//         console.log(fullname+ " your best food is not in our list, please eat well and stay safe.")
+// }
+// var fullname= prompt("What is your name?")
+// var bestsubject= prompt("What is your best subject?")
+// switch(bestsubject) {
+//     case "math":
+//         console.log(fullname+" your best subject is "+ bestsubject+ " you are most likely to become a mathematician. ")
+//         break;
+//     case "science":
+//         console.log(fullname+" your best subject is"+bestsubject+" you are most likely to become a scientist or an astronaut. ")
+//         break;
+//     case "art":
+//         console.log(fullname+" your best subject is "+bestsubject+" you are most likely to become an artist.")
+//         break;
+//     case "english":
+//         console.log(fullname+" your best subject is "+bestsubject+"you are most likely to become an english teacher or a writer.")
+//         break;
+//     case "physical activites":
+//         console.log(fullname+" your best subject is "+bestsubject+"you are most likely to become an athlete.")
+//         break;
+//     default:
+//         console.log(fullname+" your best subject is not our list, have a great school year and stay safe.")
+// }
+// var p1= document.querySelector(".p")
+// var p2= document.querySelector(".i_did_this_in_html")
+// p1.textContent= "This is Red"
+// p1.style.backgroundColor= "red"
+// p2.textContent= "I did this in Java Script"
+// var p1= document.querySelector(".red")
+// var p2= document.querySelector(".blue")
+// var p3= document.querySelector(".green")
+// p1.textContent= "This is yellow"
+// p1.style.backgroundColor= "yellow"
+// p2.textContent= "This is orange"
+// p2.style.backgroundColor= "orange"
+// p3.textContent= "This is purple"
+// p3.style.backgroundColor= "purple"
+// var cars= ["Tesla","Doge","Honda","Toyota"]
+// console.log(cars)
+// var fruit= new Array("Apple","Bannana","Grape","Strawberry")
+// console.log(fruit)
+// console.log(cars[0])
+// console.log(fruit[1])
+// cars[1]="Lesis"
+// console.log(cars[1])
+// fruit[2]="Pineapple"
+// console.log(fruit[2])
+// cars.unshift("Fiat")
+// console.log(cars)
+// fruit.push("Cherry")
+// console.log(fruit)
+// cars.pop()
+// console.log(cars)
+// fruit.shift()
+// console.log(fruit)
+// fruit.sort()
+// console.log(fruit)
+// var allarrays= cars.concat(fruit)
+// console.log(allarrays)
+// var food= ["Rice","Beans","Hamburger","Hotdog"]
+// var selected_food= food.slice(1)
+// // console.log(selected_food)
+// var food_position= food.indexOf("Hotdog")
+// console.log(food_position)
+// var person= {
+//     firstname:"Sarah",
+//     lastname:"Smith",
+//     age:11,
+//     personality:"Kind",
+//     favourite_food:"Rice",
+//     hobby:"Drawing"
+// }
+// console.log(person)
+// var animal= {
+//     animal_name: "Panda",
+//     nationality: "China",
+//     food: "Bamboo",
+//     populaton: 1864,
+//     size: "Giant Panda"
+// }
+// console.log(animal)
+// var school= new Object();
+// school.name= "School of Happy Valley";
+// school.location= "Canada";
+// school.teachers= 653;
+// school.students= 923
+// school.classrooms= 381
+// console.log(school)
+
+// var church= {
+//     church_name: "Glad Tidings Burlington",
+//     location: "Burlington",
+//     people: 3500,
+//     services: 1,
+//     opening_time: "9:00"
+// }
+// console.log(church)
+// console.log(church.location)
+// console.log(church.people)
+// console.log("Hello my name is Tikristi, I worship at "+church.church_name+". There are "+church.people+". We usally hold just "+church.services+" service. Which starts by "+church.opening_time+". You are very much welcome to join us this sunday, thank you!")
+
+// var sister= {
+//     sister_name: "Gloria",
+//     last_name: "Aderinwale",
+//     age: 11,
+//     hobby: "Coding and Drawing",
+//     favourite_food: "Rice",
+//     favourite_color: "Lime"
+// }
+// console.log(sister)
+// sister.age= 20
+// console.log("Hello my name is Tikristi, I have her sister and her name is "+sister.sister_name+" "+sister.last_name+". She is "+sister.age+" years old. She likes "+sister.hobby+", her favourite food is "+sister.favourite_food+",and her favourite color is "+sister.favourite_color+". She is a really good sister, I can't wait for you to meet her!")
+// for(var x= 0; x<101; x=x+1) {
+//     console.log(x)
+// }
+// for(var a= 0; a<101; a++) {
+//     if(a%3== 0) {
+//         console.log(a)
+//     }
+// }
+// for(var y= 0; y<51; y++) {
+//     if(y%5== 0) {
+//         console.log(y)
+//     }
+// }
+
+// var states= ["Quebec","New Brunswick","Nova Scotia","Ontario","Alberta","Manitoba","Nunavut"]
+// for(var c= 0; c<states.length; c++) {
+//     console.log(states[c])
+// }
+// var names= ["Mary","Mark","Lily","Alex","Mia","Zander","Louise","Elijah","August","Liam"]
+// for(var e= 0; e<names.length; e++) {
+//     console.log(names[e])
+// }
+// LET US CREATE OUR VARIABLES
+var firstname = document.querySelector("#firstName");
+var lastname = document.querySelector("#lastName");
+var mass = document.querySelector("#mass");
+var height = document.querySelector("#height");
+var button = document.querySelector(".btn");
+var result = document.querySelector(".Result");
+
+// LET'S ADD AND EVENT LISTENER
+button.addEventListener("click", calcBmi);
+
+// LET'S DECLARE THE FUNCTION
+function calcBmi() {
+    console.log(firstname.value)
+    // LET US CALCULATE THE BMI
+    var bmi = mass.value/(height.value*height.value)
+    // LET US SET THE BMI TO 2 DECIMAL PLACES
+    bmi = bmi.toFixed(1)
+    // LET US DO FORM VALIDATION
+    if(firstname.value=="" || lastname.value=="" || mass.value=="" || height.value==""){
+        setTimeout(() => {
+            result.textContent= "Please fill in all the blanks";
+            result.style.color="tomato";
+
+            setTimeout(() => {
+                result.textContent= ""
+            },5000)
+        },100)
+    }else {
+        if(bmi<18.5){
+            result.textContent="Hello, "+ firstname + "Your BMI value is "+bmi+ ". You fall under the underweight range. Please see a doctor and stay safe. Thank you for using Tikristi's BMI calculator."
+        } else if(bmi>18.4 && bmi<25){
+            result.textContent="Hello, "+ firstname + "Your BMI value is "+bmi+ ". You fall under the healthy weight rage. Please stay safe and thank you for using Tkristi's BMI calculator."
+        } else if(bmi>25.0 && bmi<30) {
+            result.textContent="Hello, "+ firstname +  "Your BMI value is "+bmi+ ". You fall under the overweight range. Please see a doctor and stay safe. Thank you for using Tikristi's BMI calculator."
+        } else{
+            result.textContent="Hello, "+ firstname +  "Your BMI value is "+bmi+ ". You fall under the obesity range. Please see a doctor and stay safe. Thank you for using Tikristi's BMI calculator."
+        }
+    }
+}
